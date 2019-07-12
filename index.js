572e20904d32cebbb75b10cf0e544dd46a4a9f85
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-12 09:51:05 -0400

    Added js file

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("I'm working!!");
+const button = document.querySelector("#button");
+button.addEventListener('click', (e) => {
+  document.getElementById("picture").style.display = 'block';
+})
+//title.addEventListener('mouseover', (e) => {
+//  document.getElementById("title").style.fontSize = "100px";
+}
+
+)
