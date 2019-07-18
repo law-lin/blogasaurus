5cf4d605e6bf4a9e331734381c99349b9d2ec9a9
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-18 10:37:36 -0400

    Templates and Forms added

--- main.py
+++ main.py
@@ -16,16 +16,42 @@ class KonamiCode(webapp2.RequestHandler):
     def get(self):
         templates = jinja_env.get_template('konamicode.html')
         self.response.write(templates.render())
+
 class BlogHandler(webapp2.RequestHandler):
     def get(self):
-        templates = jinja_env.get_template('new_post.html')
-        self.response.write(templates.render())
+        templates = jinja_env.get_template('templates/new_post.html')
+        post_title = self.request.get('title')
+        post_content = self.request.get('content')
+        authors_name = self.request.get('name')
+        print(post_title)
+        print(post_content)
+        print(authors_name)
+        the_variable_dict = {
+        "post_title": post_title,
+        "post_content": post_title,
+        "authors_name": authors_name,
+        }
+        self.response.write(templates.render(the_variable_dict))
+    def post(self):
+        templates = jinja_env.get_template('templates/view_post.html')
+        post_title = self.request.get('title')
+        post_content = self.request.get('content')
+        authors_name = self.request.get('name')
+        the_variable_dict = {
+        "post_title": post_title,
+        "post_content": post_title,
+        "authors_name": authors_name,
+        }
+        self.response.write(templates.render(the_variable_dict))
+
 
 app = webapp2.WSGIApplication([
     # This line routes the main url ('/')  - also know as
     # The root route - to the Fortune Handler
     ('/', Game),
-    ('/konamicode', KonamiCode)
+    ('/konamicode', KonamiCode),
+    ('/newblog', BlogHandler)
+
 
 
 ], debug=True)
